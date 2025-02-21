c2de5e82999cbe685a35175777dcad810402023e
EffexDev <effexdev@gmail.com>
2025-02-21 22:53:49 +0800

    fixed width

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import DarkmodeButton from "./components/DarkModeButton";
 import Header from "./components/Header";
 import WorkExperience from "./components/WorkExperience";
-import Skills from "./components/Skills";
 import "./index.css"
 
 export default function App() {
 
   return (
     <div className="flex justify-center h-screen px-10 py-20 dark:bg-gray-800 dark:text-white">
-      <div className="max-w-2xl">
+      <div className="w-full max-w-2xl">
         <Header />
-        <Skills />
         <WorkExperience />
         <DarkmodeButton />
       </div>
